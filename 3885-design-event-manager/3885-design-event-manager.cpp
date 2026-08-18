311c57091f972f456e7dae57316b15ce674638ac
Sakshi20-g <sakshiharitwal16@gmail.com>
2026-08-18 22:14:38 +0530

    Time: 149 ms (86.24%), Space: 292.6 MB (87.96%) - LeetHub-Neo

---
+++ 3885-design-event-manager/3885-design-event-manager.cpp
@@ -0,0 +1,43 @@
+class EventManager {
+public:
+    unordered_map<int,int> mp;
+    priority_queue<pair<int,int>> pq;
+    
+    EventManager(vector<vector<int>>& events) {
+        for(auto &e : events){
+            int id = e[0];
+            int p = e[1];
+            mp[id] = p;
+            pq.push({p, -id});
+        }
+    }
+    
+    void updatePriority(int eventId, int newPriority) {
+        mp[eventId] = newPriority;
+        pq.push({newPriority,-eventId});
+    }
+    
+    int pollHighest() {
+        while(!pq.empty()){
+            auto [p,negId] = pq.top();
+            int id = -negId;
+
+            if(mp.count(id) && mp[id]==p){
+                pq.pop();
+                mp.erase(id);
+                return id;
+                
+            }
+            pq.pop();
+            
+        }
+        return -1;
+    }
+};
+
+/**
+ * Your EventManager object will be instantiated and called as such:
+ * EventManager* obj = new EventManager(events);
+ * obj->updatePriority(eventId,newPriority);
+ * int param_2 = obj->pollHighest();
+ */
